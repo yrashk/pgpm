c77754520dbc3ebd5d0da382a2e03afcc1e5edd6
Yurii Rashkovskii <yrashk@gmail.com>
2024-10-11 13:37:57 -0700

    Problem: rubocop rebels
    It doesn't like some of my formatting.
    
    Solution: concede to rubocop

--- lib/pgpm/rpm/mock/config.rb
+++ lib/pgpm/rpm/mock/config.rb
@@ -6,11 +6,10 @@ module Pgpm
       class Config
         def initialize(name)
           @source = name
-          @path = File.absolute_path(File.join(File.dirname(__FILE__),"..","mock", "configs","#{name}.cfg"))
+          @path = File.absolute_path(File.join(File.dirname(__FILE__), "..", "mock", "configs", "#{name}.cfg"))
         end
 
         attr_reader :path
-
       end
     end
   end
