a2968c97b50cb7c8dc5113d25edc9327110c9fc9
Yurii Rashkovskii <yrashk@gmail.com>
2024-10-26 18:15:55 -0700

    Problem: Podman.run will silently eat the error
    Solution: raise an exception instead

--- lib/pgpm/podman.rb
+++ lib/pgpm/podman.rb
@@ -6,9 +6,12 @@ module Pgpm
   module Podman
     def self.run(command, unhandled_reboot_mitigation: true)
       output = `podman #{command} 2>&1`
-      if unhandled_reboot_mitigation && $CHILD_STATUS.exitstatus != 0 && output =~ /Please delete directories/
+      if $CHILD_STATUS.exitstatus != 0
+        raise StandardError, output unless unhandled_reboot_mitigation && output =~ /Please delete directories/
+
         FileUtils.rm_rf(["/run/containers/storage", "/run/libpod"])
         run(command, unhandled_reboot_mitigation: false)
+
       end
       output
     end
