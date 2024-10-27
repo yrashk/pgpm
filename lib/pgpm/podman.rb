af540d05f9ae80c562ba4352391a7e129545b457
Yurii Rashkovskii <yrashk@gmail.com>
2024-10-27 07:49:54 -0700

    Problem: incorrect handling of errors with tty-command
    It uses exceptions instead of status codes
    
    Solution: do that

--- lib/pgpm/podman.rb
+++ lib/pgpm/podman.rb
@@ -12,15 +12,14 @@ module Pgpm
         print err
       end
 
-      if result.status != 0
-        if unhandled_reboot_mitigation && result.err =~ /Please delete/
-          FileUtils.rm_rf(["/run/containers/storage", "/run/libpod"])
-          run(command, unhandled_reboot_mitigation: false)
-        end
-
-        raise StandardError, result.err
-      end
       result.out
+    rescue TTY::Command::ExitError => e
+      if unhandled_reboot_mitigation && e.message =~ /Please delete/
+        FileUtils.rm_rf(["/run/containers/storage", "/run/libpod"])
+        run(command, unhandled_reboot_mitigation: false)
+      end
+
+      raise
     end
   end
 end
