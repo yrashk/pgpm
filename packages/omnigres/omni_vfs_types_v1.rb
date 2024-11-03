a04c3954c87b3827ec59b95243c9e1d3a7202268
Yurii Rashkovskii <yrashk@gmail.com>
2024-11-03 04:46:11 -0800

    Problem: omni_vfs_types_v1 RPM fails to build
    Can't find any files for debugsource package
    
    Solution: ensure it is not treated as a native-code extension
    
    This extension is pure SQL. However, it shares the directory with
    `omni_vfs` which has C code, and it was (wrongly) inferred to be a
    native-code extension.

--- packages/omnigres/omni_vfs_types_v1.rb
+++ packages/omnigres/omni_vfs_types_v1.rb
@@ -7,5 +7,11 @@ module Omnigres
     def summary
       "Virtual File System API"
     end
+
+    def native?
+      # This extension shares the directory with `omni_vfs` which has .c files,
+      # but it is not a native-code extension itself.
+      false
+    end
   end
 end
