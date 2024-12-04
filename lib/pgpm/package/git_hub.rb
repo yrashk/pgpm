948baaa2b9995cb21dda8b897300c69b3ea56bc6
Yurii Rashkovskii <yrashk@gmail.com>
2024-12-04 10:08:59 +0700

    Problem: source_url_directory_name is not always correct
    It is implied from extension name, however, that is not always true.
    
    Solution: make it implied from a respective source, such as github
    
    This also corrects how we inject GitHub-related methods (only when
    GitHub method is used)

--- lib/pgpm/package/git_hub.rb
+++ lib/pgpm/package/git_hub.rb
@@ -7,11 +7,18 @@ module Pgpm
     module GitHub
       Config = Data.define(:name, :download_version_tags)
 
-      def sources
-        commit = version_git_tag || version_git_commit
-        [Pgpm::OnDemandFile.new("#{version}.tar.gz", lambda {
-          URI.open("https://github.com/#{self.class.github_config.name}/archive/#{commit}.tar.gz")
-        })] + super
+      module Methods
+        def sources
+          commit = version_git_tag || version_git_commit
+          [Pgpm::OnDemandFile.new("#{version}.tar.gz", lambda {
+            URI.open("https://github.com/#{self.class.github_config.name}/archive/#{commit}.tar.gz")
+          })] + super
+        end
+
+        def source_url_directory_name
+          commit = version_git_tag || version_git_commit
+          "#{self.class.github_config.name.split("/").last}-#{commit}"
+        end
       end
 
       module ClassMethods
@@ -20,6 +27,7 @@ module Pgpm
         def github(name, download_version_tags: true)
           @github_config = Config.new(name:, download_version_tags:)
           include Pgpm::Package::Git
+          include Methods
           git "https://github.com/#{@github_config.name}", download_version_tags:
         end
       end
